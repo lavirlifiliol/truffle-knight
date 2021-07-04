548c90518d529ed1d268be21f9547283aba0a324
lavirlifiliol <adam.hrbac@gmail.com>
2021-07-04 22:01:48 +0200

    Fix the Shell specialization

--- src/main/java/xyz/lakmatiol/knight/ast/unary/Shell.java
+++ src/main/java/xyz/lakmatiol/knight/ast/unary/Shell.java
@@ -1,26 +1,33 @@
 package xyz.lakmatiol.knight.ast.unary;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import xyz.lakmatiol.knight.ast.Types;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 
 @NodeInfo(shortName = "`")
 public abstract class Shell extends Unary{
+    @Specialization
     public String call(String a) {
+        return doExec(a);
+    }
+    @Specialization
+    public String callGen(Object a) {
+        return call(Types.stringCoerce(a));
+    }
+    @CompilerDirectives.TruffleBoundary
+    public String doExec(String in) {
         try {
             var r = Runtime.getRuntime();
-            var p = r.exec(a);
+            var p = r.exec(in);
             return new String(p.getInputStream().readAllBytes());
         }
         catch(IOException e) {
             throw new RuntimeException(e);
         }
-    }
-    @Specialization
-    public String callGen(Object a) {
-        return call(Types.stringCoerce(a));
     }
 }
