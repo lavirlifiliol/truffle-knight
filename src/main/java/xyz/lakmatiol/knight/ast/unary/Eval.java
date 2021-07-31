5ba3c7ec1ea9478890f32ec11ebe3fba29e5a9e0
lavirlifiliol <adam.hrbac@gmail.com>
2021-07-31 18:37:03 +0200

    Change eval to call the parser directly

--- src/main/java/xyz/lakmatiol/knight/ast/unary/Eval.java
+++ src/main/java/xyz/lakmatiol/knight/ast/unary/Eval.java
@@ -2,6 +2,8 @@ package xyz.lakmatiol.knight.ast.unary;
 
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.CachedContext;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -10,19 +12,23 @@ import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.source.Source;
 import xyz.lakmatiol.knight.Context;
 import xyz.lakmatiol.knight.Knight;
+import xyz.lakmatiol.knight.Parser;
 import xyz.lakmatiol.knight.ast.Types;
+
+import java.io.IOException;
+import java.io.StringReader;
 
 @NodeInfo(shortName = "EVAL")
 public abstract class Eval extends Unary {
     @Specialization(
-            guards={"stringEquals(s, cacheS)"}, limit = "3"
+            guards = {"stringEquals(s, cacheS)"}, limit = "3"
     )
     public Object evalCached(
             String s,
             @Cached("s") String cacheS,
             @CachedContext(Knight.class) Context c,
             @Cached("create(parse(s, c))") DirectCallNode n
-            ) {
+    ) {
         return n.call();
     }
 
@@ -33,14 +39,21 @@ public abstract class Eval extends Unary {
     }
 
     protected CallTarget parse(String code, Context c) {
-        var src = Source.newBuilder("Knight", code, "(eval)").build();
-        return c.env.parsePublic(src);
+//        var src = Source.newBuilder("Knight", code, "(eval)").build();
+//        return c.env.parsePublic(src);
+        try {
+            return Truffle.getRuntime().createCallTarget(new Parser(new StringReader(code), null).parse());
+        } catch (IOException e) {
+            Types.errorUB();
+            return null;
+        }
     }
 
     @Specialization
     public Object evalGen(Object o, @CachedContext(Knight.class) Context c) {
         return parse(Types.stringCoerce(o), c).call();
     }
+
     public boolean stringEquals(String l, String r) {
         return l.equals(r);
     }
